8480fec89135fb4c7ec853e95c952144ec619c51
Bui Duy Tung <tungfami91@gmail.com>
2013-05-22 12:07:56 +0300

    123
    Some little start up

--- gen/fi/metropolia/VIOPE/physixviopeandroid/R.java
+++ gen/fi/metropolia/VIOPE/physixviopeandroid/R.java
@@ -24,30 +24,54 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int BtnGoToGeo=0x7f080002;
-        public static final int BtnGoToProjectile=0x7f080005;
-        public static final int BtnGoToTrigonometry=0x7f080004;
-        public static final int BtnGoToVec=0x7f080003;
-        public static final int action_settings=0x7f080006;
+        public static final int BtnGoToGeo=0x7f080008;
+        public static final int BtnGoToProjectile=0x7f08000b;
+        public static final int BtnGoToTrigonometry=0x7f08000a;
+        public static final int BtnGoToVec=0x7f080009;
+        public static final int action_settings=0x7f08000c;
+        public static final int btnBrick=0x7f080007;
+        public static final int btnCircle=0x7f080004;
+        public static final int btnPyramid=0x7f080003;
+        public static final int btnQuad=0x7f080006;
+        public static final int btnSphere=0x7f080005;
+        public static final int btnTriangle=0x7f080002;
         public static final int textView1=0x7f080000;
         public static final int textView2=0x7f080001;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_geometry=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_projectile=0x7f030002;
+        public static final int activity_trigonometry=0x7f030003;
+        public static final int activity_vector=0x7f030004;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int geometry=0x7f070000;
+        public static final int main=0x7f070001;
+        public static final int projectile=0x7f070002;
+        public static final int trigonometry=0x7f070003;
+        public static final int vector=0x7f070004;
     }
     public static final class string {
+        public static final int Brick=0x7f05000f;
+        public static final int Circle=0x7f05000b;
         public static final int Copyright=0x7f050004;
         public static final int Geometry=0x7f050006;
         public static final int Projectile=0x7f050007;
+        public static final int Pyramid=0x7f05000d;
+        public static final int Quad=0x7f05000c;
+        public static final int Sphere=0x7f05000e;
+        public static final int Triangle=0x7f05000a;
         public static final int Trigonometry=0x7f050008;
         public static final int Vector=0x7f050005;
         public static final int Welcome=0x7f050003;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_geometry=0x7f050009;
+        public static final int title_activity_projectile=0x7f050012;
+        public static final int title_activity_trigonometry=0x7f050011;
+        public static final int title_activity_vector=0x7f050010;
     }
     public static final class style {
         /** 
--- src/fi/metropolia/VIOPE/physixviopeandroid/MainActivity.java
+++ src/fi/metropolia/VIOPE/physixviopeandroid/MainActivity.java
@@ -2,7 +2,9 @@ package fi.metropolia.VIOPE.physixviopeandroid;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -18,5 +20,21 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	
+	public void goToGeoPage(View view){
+		Intent intent = new Intent(this,GeometryActivity.class);
+		startActivity(intent);
+	}
+	public void goToVecPage(View view){
+		Intent intent = new Intent(this,VectorActivity.class);
+		startActivity(intent);
+	}
+	public void goToTrigonometryPage(View view){
+		Intent intent = new Intent(this,TrigonometryActivity.class);
+		startActivity(intent);
+	}
+	public void goToProjectilePage(View view){
+		Intent intent = new Intent(this,GeometryActivity.class);
+		startActivity(intent);
+	}
 }
